82de89465ddb9207517c9b19e46cc23c245ffd69
willianlode <willianlode@hotmail.com>
2025-07-02 13:34:52 -0300

    Implemented currency formating to internet banking project

--- internet-banking/contaBanco/src/Conta.java
+++ internet-banking/contaBanco/src/Conta.java
@@ -1,9 +1,18 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Conta {
     private double saldo;
     private double chequeEspecial;
     private double saldoCheque;
+
+    public String formataMoeda(double valor){
+        NumberFormat formata=NumberFormat.getCurrencyInstance();
+        BigDecimal bigDecimal=new BigDecimal(valor);
+        String valorConvertido=formata.format(bigDecimal);
+        return valorConvertido;
+    }
 
     public Conta(double saldo){
         this.saldo=saldo;
@@ -18,34 +27,34 @@ public class Conta {
         consultaCheque();
     }
     public void consultaSaldo(){
-        System.out.println("O seu saldo é " + saldo);
+        System.out.println("O seu saldo é " + formataMoeda(saldo));
     }
     public void consultaCheque(){
-        System.out.println("Você tem um cheque especial de " + chequeEspecial);
+        System.out.println("Você tem um cheque especial de " + formataMoeda(chequeEspecial));
     }
     public void verificaCheque(){
-        System.out.println("Você possui R$ " + this.saldoCheque + " disponíveis de um total de R$ " + this.chequeEspecial + " de cheque especial.");
+        System.out.println("Você possui " + formataMoeda(saldoCheque) + " disponíveis de um total de " + formataMoeda(chequeEspecial) + " de cheque especial.");
     }
     public void pagaBoleto(double valorBoleto){
-        if(valorBoleto<=this.saldo){
-           this.saldo=this.saldo-valorBoleto; 
-           System.out.println("Pagamento realizado! Saldo disponível: " + this.saldo);
+        if(valorBoleto<=saldo){
+           saldo=saldo-valorBoleto; 
+           System.out.println("Pagamento realizado! Saldo disponível: " + formataMoeda(saldo));
         }else{
             System.out.println("Saldo insuficiente! Valor boleto:");
-            System.out.println("R$ "+valorBoleto);
+            System.out.println(""+formataMoeda(valorBoleto));
             consultaSaldo();
-            System.out.println("Faltam R$ "+ (valorBoleto-this.saldo));
+            System.out.println("Faltam "+ formataMoeda(valorBoleto-saldo));
             verificaCheque();
             System.out.println("Deseja usar o cheque especial? *taxa de 20% aplicável");
             System.out.println("[y/n]?");
             Scanner scanner=new Scanner(System.in);
             String resposta=scanner.next();
             if(resposta.toLowerCase().equals("y")){
-                this.saldoCheque=this.chequeEspecial-(valorBoleto-this.saldo);
-                this.saldo=0;
-                System.out.println("Pagamento realizado! Saldo disponível: " + this.saldo);
-                System.out.println("Cheque especial disponível: " + this.saldoCheque);
-                System.out.println("Taxa a ser cobrada: " + 0.2*(this.chequeEspecial-this.saldoCheque));
+                saldoCheque=chequeEspecial-(valorBoleto-saldo);
+                saldo=0;
+                System.out.println("Pagamento realizado! Saldo disponível: " + formataMoeda(saldo));
+                System.out.println("Cheque especial disponível: " + formataMoeda(saldoCheque));
+                System.out.println("Taxa a ser cobrada: " + formataMoeda(0.2*(chequeEspecial-saldoCheque)));
             }else if(resposta.toLowerCase().equals("n")){
                 System.out.println("Pagamento não realizado.");
             }
@@ -55,27 +64,35 @@ public class Conta {
     public void sacaDinheiro(double valorSaque){
         if(valorSaque<=saldo){
             this.saldo=saldo-valorSaque;
-            System.out.println("Pagamento realizado! Saldo disponível: " + this.saldo);
+            System.out.println("Pagamento realizado! Saldo disponível: " + formataMoeda(saldo));
         }else{
             System.out.println("Saldo insuficiente! Valor saque:");
-            System.out.println("R$ "+valorSaque);
+            System.out.println(""+formataMoeda(valorSaque));
             consultaSaldo();
-            System.out.println("Faltam R$ "+ (valorSaque-this.saldo));
+            System.out.println("Faltam "+ formataMoeda(valorSaque-this.saldo));
             verificaCheque();
             System.out.println("Deseja usar o cheque especial? *taxa de 20% aplicável");
             System.out.println("[y/n]?");
             Scanner scanner=new Scanner(System.in);
             String resposta=scanner.next();
             if(resposta.toLowerCase().equals("y")){
-                this.saldoCheque=this.chequeEspecial-(valorSaque-this.saldo);
-                this.saldo=0;
-                System.out.println("Saque realizado! Saldo disponível: " + this.saldo);
-                System.out.println("Cheque especial disponível: " + this.saldoCheque);
-                System.out.println("Taxa a ser cobrada: " + 0.2*(this.chequeEspecial-this.saldoCheque));
+                saldoCheque=chequeEspecial-(valorSaque-saldo);
+                saldo=0;
+                System.out.println("Saque realizado! Saldo disponível: " + formataMoeda(saldo));
+                System.out.println("Cheque especial disponível: " + formataMoeda(saldoCheque));
+                System.out.println("Taxa a ser cobrada: " + formataMoeda(0.2*(chequeEspecial-saldoCheque)));
             }else if(resposta.toLowerCase().equals("n")){
                 System.out.println("Pagamento não realizado.");
             }
             scanner.close();
         }
+    }
+    public void depositaDinheiro(double valorDeposito){
+        if(saldoCheque<chequeEspecial){
+            saldo=saldo+valorDeposito-(chequeEspecial-saldoCheque)*1.2;
+        } else{
+            saldo=saldo+valorDeposito;
+        }
+        System.out.println(""+ formataMoeda(valorDeposito) +" Depositado com sucesso! Seu saldo é " + formataMoeda(this.saldo));
     }
 }
--- internet-banking/contaBanco/src/TerminalBancario.java
+++ internet-banking/contaBanco/src/TerminalBancario.java
@@ -2,8 +2,9 @@ public class TerminalBancario {
     public static void main(String[] args) throws Exception {
         System.out.println("\nBem-vindo ao Meu Banco!");
         Conta novaConta=new Conta(550);
-        novaConta.pagaBoleto(125.25);
+        novaConta.pagaBoleto(125.28);
         novaConta.sacaDinheiro(250);
         novaConta.pagaBoleto(325);
+        novaConta.depositaDinheiro(275.35);
     }
 }
