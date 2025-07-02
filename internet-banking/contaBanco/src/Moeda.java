ccd31f44d87c1c8f1f753517be335a96a8022afd
willianlode <willianlode@hotmail.com>
2025-07-02 18:56:14 -0300

    abstract class bigdecimal fix

--- internet-banking/contaBanco/src/Moeda.java
+++ internet-banking/contaBanco/src/Moeda.java
@@ -1,12 +1,9 @@
-import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 public abstract class Moeda {
 
     public static String formataMoeda(double valor){
         NumberFormat formata=NumberFormat.getCurrencyInstance();
-        BigDecimal bigDecimal=new BigDecimal(valor);
-        String valorConvertido=formata.format(bigDecimal);
-        return valorConvertido;
+        return formata.format(valor);
     }
 }
