8b6ece1a9c3b31e12907eb821755a7ac7db94df5
willianlode <willianlode@hotmail.com>
2025-07-03 15:57:36 -0300

    Erased print test

--- internet-banking/contaBanco/src/TerminalBancario.java
+++ internet-banking/contaBanco/src/TerminalBancario.java
@@ -1,7 +1,6 @@
 public class TerminalBancario {
     public static void main(String[] args) throws Exception {
         System.out.println("\nBem-vindo ao Meu Banco!");
-        System.out.println(Moeda.formataMoeda(325.75));
         Conta novaConta=new Conta(550);
         novaConta.pagaBoleto(125.28);
         novaConta.sacaDinheiro(250);
